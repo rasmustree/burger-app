9aa4774f781c0eb554da8848410b02e25bf5d236
Daniel Abu Ramadan <daab24@student.bth.se>
2024-10-10 14:43:07 +0200

    updatera server.js

--- server.js
+++ server.js
@@ -1,57 +1,17 @@
 const express = require('express'); //import express
 const app = express(); //skapa instans av express
 const PORT = 3000;
-
-const Burgers = [
-    {name: "Bacon burgare"},
-    {name: "Cheese burgare"}
-]
-
-const Sides = [
-    {name: "Pommes"},
-    {name: "20x Nuggets"}
-]
-
-const Combos = [
-    {name: "Bacon burgare meny"},
-    {name: "Cheese burgare meny"},
-    {name: "20x Nuggets meny"}
-]
-
-const drinks = [
-    {name: "Cola"},
-    {name: "Fanta"},
-    {name: "Sprite"}
-]
-
+const path = require('path')
 
 app.get('/', (req, res) => {
     res.send('Hello World!'); //lyssna på request som görs till URL(localhost:3000)
     console.log('User has entered BurgerOrderer')
 })
 
-app.get('/burgers/', (req, res) => {
-    res.send('Here are the burgers');
-    console.log('User entered burgers')
-    console.log(Burgers)
-})
-
-app.get('/combos/', (req, res) => {
-    res.send('Here are the combos');
-    console.log('User entered combos');
-    console.log(Combos)
-})
-
-app.get('/drinks/', (req, res) => {
-    res.send('Here are the drinks');
-    console.log('User entered drinks');
-    console.log(drinks)
-})
-
-app.get('/sides/', (req, res) => {
-    res.send('Here are the sides');
-    console.log('User entered sides');
-    console.log(Sides)
+app.get('/items', (req, res) => {
+    const filePath = path.join(__dirname, 'frontend_ordering', 'frontend_objects.html')
+    console.log('User entered items')
+    res.sendFile(filePath)
 })
 
 app.get('/checkout/', (req, res) => {
