b6a2639c97ed339ecfbdff1778817d55b9cb2597
aldinjusufagic <aldin_jusufagic@hotmail.com>
2024-10-15 15:27:39 +0200

    fixed some problems with ingredientsText

--- Containers/BurgerOrderer/Frontend_operations.js
+++ Containers/BurgerOrderer/Frontend_operations.js
@@ -4,26 +4,37 @@ const burgers = [
     ["Kycklingburgare", "Bröd", "Kyckling", "Ost", "Sallad", "Ketchup"]
 ]
 const optionsContainer = document.getElementById("options");
+const ingredientsContainer = document.getElementById("ingredients")
 
 function onStart(){
     for(let i = 0; i < burgers.length; i++ ){
         var button = document.createElement("button");
         button.textContent = burgers[i][0];
-        button.addEventListener("click", onButtonClick);
+        button.addEventListener("click", onItemButtonClick);
         optionsContainer.appendChild(button);
     }
 }
-function onButtonClick(e){
+function onItemButtonClick(e){
     console.log(e.target.textContent + " has been clicked!");
     var ingredientsText = document.createElement("p");
-    ingredientsText.textContent = "";
+    let existingParagraph = ingredientsContainer.querySelector("p")
+    if(existingParagraph){
+        existingParagraph.remove()
+    }
     for (let i = 0; i < burgers.length; i++) {
         if (burgers[i][0] === e.target.textContent){
             for (let j = 1; j < burgers[i].length; j++) {
-                ingredientsText.textContent += `${burgers[i][j]}\n`
+                ingredientsText.innerHTML += `${burgers[i][j]}\n`
             }
+            var button = document.createElement("button")
+            button.textContent = "Add To Order"
+            button.addEventListener("click", onOrderButtonClick)
             break;
         }
     }
-    optionsContainer.appendChild(ingredientsText)
+    ingredientsContainer.appendChild(ingredientsText)
+    ingredientsContainer.appendChild(button)
+}
+function onOrderButtonClick(e){
+    console.log("Order Button Clicked!")
 }
