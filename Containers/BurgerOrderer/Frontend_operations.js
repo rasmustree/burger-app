6c5dc99a626e30fd9121cc4daf95561111144a7a
rasmustree <rasmusangarp@hotmail.se>
2024-10-18 08:35:37 +0200

    Update Frontend_operations.js

--- Containers/BurgerOrderer/Frontend_operations.js
+++ Containers/BurgerOrderer/Frontend_operations.js
@@ -1,8 +1,29 @@
-// temp array
-const burgers = [
-    ["Cheeseburgare", "Bröd", "Kött", "Ost", "Sallad", "Ketchup"],
-    ["Kycklingburgare", "Bröd", "Kyckling", "Ost", "Sallad", "Ketchup"]
-]
+//import mysql from 'mysql'
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "burger",
+    database: "MenuStoreDB"
+});
+const temp_burgers = []
+con.connect(function(err) {
+    if(err) throw err;
+    con.query("SELCT * FROM burgers", function(err, result, fields) {
+        if(err) throw err;
+        temp_burgers = result;
+    });
+});
+
+
+
+// temp array (kept incase shit hits the fan)
+//const burgers = [
+//    ["Cheeseburgare", "Bröd", "Kött", "Ost", "Sallad", "Ketchup"],
+//    ["Kycklingburgare", "Bröd", "Kyckling", "Ost", "Sallad", "Ketchup"]
+//]
 const optionsContainer = document.getElementById("options");
 const ingredientsContainer = document.getElementById("ingredients");
 const orderButtonContainer = document.getElementById("orderButton");
